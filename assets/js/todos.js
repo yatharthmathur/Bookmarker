66402d60eb592bd39be04b49fd49488a766f97d3
Vin-dictive <34976549+Vin-dictive@users.noreply.github.com>
2020-04-30 20:58:33 +0530

    Changed the resized the images

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,15 +2,34 @@
 		// var new_bookmark_name = prompt("Enter The new bookmark name:");
 		if($("#new_list_name").val()==""){
 			console.log($("#new_list_name").val());
+			$("#new_list_name").addClass("border-danger");
 		}
 		else{
 			var new_bookmark_name = $("#new_list_name").val();
 			var v = new_bookmark_name.replace(/\s/g, '');
 			console.log($("#new_list_name").val());
-			$("#add_bookmark").append(`<div class="card border-dark mb-3" id="${v}" style="max-width: 18rem;"><div class="card-header"><h4>${new_bookmark_name}</h4></div><div class="card-body text-dark" id="dynamic"><ul id="${v}_list"></ul></div><div class="card-footer bg-transparent border-dark"><input type="text" placeholder="Add New Todo"></div></div>`);
+			$("#add_bookmark").append(`<div class="card border-dark mb-3" id="${v}" style="max-width: 18rem;"><div class="card-header"><h4><span><img src="assets/images/trash.png" alt=""></span>${new_bookmark_name}</h4></button>
+			</div><div class="card-body text-dark" id="dynamic"><ul id="${v}_list"></ul></div><div class="card-footer bg-transparent border-dark"><input type="text" placeholder="Add New Todo"></div></div>`);
 		}
-		$("#new_list_name").val("")
+		$("#new_list_name").val("");
+		$("#new_list_name").removeClass("border-danger");
 
+	});
+
+	$("#new_list_name").keypress(function(e){
+		if(e.which == 13){
+				// var new_bookmark_name = prompt("Enter The new bookmark name:");
+			if($("#new_list_name").val()==""){
+				console.log($("#new_list_name").val());
+			}
+			else{
+				var new_bookmark_name = $("#new_list_name").val();
+				var v = new_bookmark_name.replace(/\s/g, '');
+				console.log($("#new_list_name").val());
+				$("#add_bookmark").append(`<div class="card border-dark" id="${v}"><div class="card-header"><h4><span><img src="assets/images/trash.png" alt=""></span>${new_bookmark_name}</h4></button></div><div class="card-body text-dark" id="dynamic"><ul id="${v}_list"></ul></div><div class="card-footer bg-transparent border-dark"><input type="text" placeholder="Add New Todo"></div></div>`);
+			}
+			$("#new_list_name").val("")
+		}
 	});
 	
 	$("section").on("click", "li", function(){
@@ -20,6 +39,14 @@
 	//Click on X to delete Todo
 	$("section").on("click", "span", function(event){
 		$(this).parent().fadeOut(500,function(){
+			$(this).remove();
+		});
+		event.stopPropagation();
+	});
+
+	//Click on X to delete Todo Big list
+	$("section").on("click", "h4 span", function(event){
+		$(this).parent().parent().parent().fadeOut(500,function(){
 			$(this).remove();
 		});
 		event.stopPropagation();
