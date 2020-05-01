611c71a89f7130af2cdca21bc8ef98a96b54dd43
Vin-dictive <34976549+Vin-dictive@users.noreply.github.com>
2020-05-01 15:05:24 +0530

    Added a feature where links are detected auto
    Also created a separate tab for Bookmarks for custom testing
    It has been commented out for now

---
+++ db.js
@@ -0,0 +1,83 @@
+window.$ = window.jquery = require('jquery');
+require('popper.js');
+require('bootstrap');
+const bcrypt = require('bcryptjs');
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb+srv://yatharth:yatharth@123@cluster0-p0dyy.mongodb.net/test?retryWrites=true&w=majority";
+const dbName = 'bookmarker';
+const client = new MongoClient(url);
+
+function readyFunction(){
+    if(localStorage.length == 0)
+        window.location.replace("login.html");
+};
+readyFunction();
+
+function cleardb(){
+    var data = {};
+    data['username'] = localStorage.username;
+    client.connect(function(err, client){
+        if(err)
+            throw err;
+        const db = client.db(dbName);
+        const userCollection = db.collection('bookmark');
+        // userCollection.save({'username':data['username'],'bookmark_contents':data['htmlContent']});
+        userCollection.remove({'username':data['username']});
+        console.log("Deletion complete");
+    });
+}
+
+
+function signOut(){
+    event.preventDefault();
+    var data = {};
+    data['username'] = localStorage.username;
+    data['htmlContent'] = $("#add_bookmark").html();
+    console.log(data['htmlContent']);
+
+    client.connect(function(err, client){
+        if(err)
+            throw err;
+        const db = client.db(dbName);
+        const userCollection = db.collection('bookmark');
+        // userCollection.save({'username':data['username'],'bookmark_contents':data['htmlContent']});
+        userCollection.update({'username':data['username']},{$set:{'bookmark_contents':data['htmlContent']}},{ upsert: true});
+        console.log("Update complete");
+    });
+    localStorage.clear();
+    window.location.replace('login.html')
+}
+
+$(document).ready(function(){
+    var data = {};
+    data['username'] = localStorage.username;
+    client.connect(function(err, client){
+        if(err)
+            throw err;
+        const db = client.db(dbName);
+        const userCollection = db.collection('bookmark');
+        userCollection.findOne({'username':data['username']},function(err,result){
+            if(err) throw err;
+            console.log(result.bookmark_contents);
+            $("#add_bookmark").html(result.bookmark_contents);
+        });
+
+    });
+
+    $("#Save_bookmark").on( "submit", function( event ) {
+        event.preventDefault();
+        data['htmlContent'] = $("#add_bookmark").html();
+        console.log(data['htmlContent']);
+
+
+        client.connect(function(err, client){
+            if(err)
+                throw err;
+            const db = client.db(dbName);
+            const userCollection = db.collection('bookmark');
+            // userCollection.save({'username':data['username'],'bookmark_contents':data['htmlContent']});
+            userCollection.update({'username':data['username']},{$set:{'bookmark_contents':data['htmlContent']}},{ upsert: true});
+            console.log("Update complete");
+        });
+    });
+});
